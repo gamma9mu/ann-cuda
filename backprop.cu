e726d7808c802e81e31abea7513f83dd0ec52a00
Brian Guthrie <baguthrie@sbcglobal.net>
2013-04-23 21:02:19 -0500

    Start work on NN kernel.

--- backprop.cu
+++ backprop.cu
@@ -2,7 +2,20 @@
 #include <cuda_runtime_api.h>
 
 extern "C" {
+#include "fforward.h"
 #include "backprop.h"
 }
 
+__global__ void
+backprop(float *data, int size, int count,
+        float *w_ih, float *w_ho,
+        float *expected)
+{
 
+    __shared__ float w_out[OUTPUT_SIZE][HIDDEN_SIZE];
+    __shared__ float w_hid[HIDDEN_SIZE][INPUT_SIZE];
+    __shared__ float out[OUTPUT_SIZE];
+
+    int tx = threadIdx.x;
+
+}
