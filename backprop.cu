535ba1c2762a674f565f507357db14065b003d07
Brian Guthrie <baguthrie@sbcglobal.net>
2013-04-23 22:56:54 -0500

    Add kernel to run the ANN.

--- backprop.cu
+++ backprop.cu
@@ -13,7 +13,7 @@ sigmoid(float x) {
 /* Performs backpropagation learning on a feed forward neural netowrk.
  * data         column-major, 2-d array of inputs (each of INPUT_SIZE length)
  * count        the total number of items in data
- * expected     column-major, 2-d array out expected output values (each of
+ * expected     column-major, 2-d array of expected output values (each of
  *              OUTPUT_SIZE length)
  * w_ih         column-major weight matrix for the hidden layer
  * theta_h      activation weights of the hidden layer
@@ -145,16 +145,13 @@ backprop(float *data, int count, float *expected,
 /* Evaluates an ANN's sum of squared errors.
  * data         column-major, 2-d array of inputs (each of INPUT_SIZE length)
  * count        the total number of items in data
- * expected     column-major, 2-d array out expected output values (each of
+ * expected     column-major, 2-d array of expected output values (each of
  *              OUTPUT_SIZE length)
  * w_ih         column-major weight matrix for the hidden layer
  * theta_h      activation weights of the hidden layer
  * w_ho         column-major weight matrix for the output layer
  * theta_o      activation weights of the output layer
  * sse          address in global memory to store the SSE into
- *
- * The updated weight matrices will be copied over their previous values in
- * device global memory after each of the items in data has been processed.
  *
  * This kernel should be called with 1 block, having a single dimension of
  * threads, where the number of threads is the maximum of the number of hidden
@@ -243,5 +240,88 @@ evaluate(float *data, int count, float *expected,
         for (int i = 0; i < OUTPUT_SIZE; ++i)
             errsum += errors[i];
         *sse = errsum;
+    }
+}
+
+/* Runs an ANN on a series of inputs.
+ * data         column-major, 2-d array of inputs (each of INPUT_SIZE length)
+ * count        the total number of items in data
+ * w_ih         column-major weight matrix for the hidden layer
+ * theta_h      activation weights of the hidden layer
+ * w_ho         column-major weight matrix for the output layer
+ * theta_o      activation weights of the output layer
+ * output       column-major, 2-d array to hold the output values (each of
+ *              OUTPUT_SIZE length)
+ *
+ * This kernel should be called with 1 block, having a single dimension of
+ * threads, where the number of threads is the maximum of the number of hidden
+ * layer neurons and the number of output layer neurons.
+ */
+__global__ void
+run(float *data, int count, float *expected,
+        float *w_ih, float *theta_h, float *w_ho, float *theta_o,
+        float *output)
+{
+    /* Hidden layer weights */
+    __shared__ float w_hid[HIDDEN_SIZE][INPUT_SIZE];
+    __shared__ float th_h[HIDDEN_SIZE];
+
+    /* Output layer weights */
+    __shared__ float w_out[OUTPUT_SIZE][HIDDEN_SIZE];
+    __shared__ float th_o[HIDDEN_SIZE];
+
+    /* Layer output values */
+    __shared__ float hid[HIDDEN_SIZE];
+    __shared__ float out[OUTPUT_SIZE];
+
+    /* Input data */
+    __shared__ float input[INPUT_SIZE];
+
+    int tx = threadIdx.x;
+
+    /* Load the hidden layer's theta values and weight matrix. */
+    if (tx < HIDDEN_SIZE) {
+        th_h[tx] = theta_h[tx];
+        for (int i = 0; i < INPUT_SIZE; ++i)
+            w_hid[tx][i] = w_ih[tx + (INPUT_SIZE * i)];
+    }
+
+    /* Load the output layer's theta values and weight matrix. */
+    if (tx < OUTPUT_SIZE) {
+        th_o[tx] = theta_o[tx];
+        for (int i = 0; i < HIDDEN_SIZE; ++i)
+            w_out[tx][i] = w_ho[tx + (OUTPUT_SIZE * i)];
+    }
+
+    /* Process each piece of input data. */
+    for (int i = 0; i < count; ++i) {
+        /* Load the data item. */
+        if (tx < INPUT_SIZE)
+            input[tx] = data[tx + (i * INPUT_SIZE)];
+
+        __syncthreads();
+        
+        /* Propagate through the hidden layer. */
+        if (tx < HIDDEN_SIZE) {
+            hid[tx] = 0.0f;
+            for (int j = 0; j < INPUT_SIZE; ++j)
+                hid[tx] += input[j] * w_hid[tx][j];
+            hid[tx] -= th_h[tx];
+            hid[tx] = sigmoid(hid[tx]);
+        }
+        
+        __syncthreads();
+
+        /* Propagate through the output layer and write the results to device
+         * global memory. */
+        if (tx < OUTPUT_SIZE) {
+            out[tx] = 0.0f;
+            for (int j = 0; j < HIDDEN_SIZE; ++j)
+                out[tx] += hid[j] * w_out[tx][j];
+            out[tx] -= th_o[tx];
+            output[tx + (OUTPUT_SIZE * i)] = sigmoid(out[tx]);
+        }
+
+        __syncthreads();
     }
 }
