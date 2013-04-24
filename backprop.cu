88e7cf7149e894dd0ca0afed6ae5d0d045ca709f
Brian Guthrie <baguthrie@sbcglobal.net>
2013-04-23 22:21:22 -0500

    Add backpropagation code to kernel.

--- backprop.cu
+++ backprop.cu
@@ -14,7 +14,7 @@ __global__ void
 backprop(float *data, int count,
         float *w_ih, float *w_ho,
         float *theta_h, float *theta_o,
-        float *expected)
+        float *expected, float rate)
 {
     /* Hidden layer weights */
     __shared__ float w_hid[HIDDEN_SIZE][INPUT_SIZE];
@@ -25,8 +25,10 @@ backprop(float *data, int count,
     __shared__ float th_o[HIDDEN_SIZE];
 
     /* Layer output values */
-    __shared__ float hid[OUTPUT_SIZE];
+    __shared__ float hid[HIDDEN_SIZE];
     __shared__ float out[OUTPUT_SIZE];
+    __shared__ float err[OUTPUT_SIZE];
+    __shared__ float delta[OUTPUT_SIZE];
 
     /* Input data */
     __shared__ float input[INPUT_SIZE];
@@ -52,9 +54,10 @@ backprop(float *data, int count,
         /* Load the data item. */
         if (tx < INPUT_SIZE)
             input[tx] = data[tx + (i * INPUT_SIZE)];
+
         __syncthreads();
         
-        /* Propogate through the hidden layer. */
+        /* Propagate through the hidden layer. */
         if (tx < HIDDEN_SIZE) {
             hid[tx] = 0.0f;
             for (int j = 0; j < INPUT_SIZE; ++j)
@@ -62,6 +65,7 @@ backprop(float *data, int count,
             hid[tx] -= th_h[tx];
             hid[tx] = sigmoid(hid[tx]);
         }
+        
         __syncthreads();
 
         /* Propagate through the output layer. */
@@ -72,7 +76,51 @@ backprop(float *data, int count,
             out[tx] -= th_o[tx];
             out[tx] = sigmoid(out[tx]);
         }
+
         __syncthreads();
 
+        /* Backpropagation starts here. */
+
+        /* Calculate the error deltas for the output layer. */
+        if (tx < OUTPUT_SIZE) {
+            err[tx] = expected[tx + (OUTPUT_SIZE * i)] - out[tx];
+            delta[tx] = out[tx] * (1 - out[tx]) * err[tx];
+        }
+
+        __syncthreads();
+
+        /* Calculate the error deltas for the hidden layer and update the
+         * weights. */
+        if (tx < HIDDEN_SIZE) {
+            float hdelta = 0.0f;
+            for (int j = 0; j < OUTPUT_SIZE; ++j)
+                hdelta += delta[j] * hid[tx];
+            hdelta *= rate;
+            for (int j = 0; j < INPUT_SIZE; ++j)
+                w_hid[tx][j] += hdelta * input[j];
+        }
+
+        __syncthreads();
+
+        /* Update the input weights of the output layer.  This section cannot be
+         * combined above.  The changes would affect the hidden layer updates.
+         */
+        if (tx < OUTPUT_SIZE)
+            for (int j = 0; j < HIDDEN_SIZE; ++j)
+                w_out[tx][j] += rate * hid[j] * delta[tx];
+
+        __syncthreads();
+    }
+
+    /* Copy the hidden layer's weight matrix out to global memory. */
+    if (tx < HIDDEN_SIZE) {
+        for (int i = 0; i < INPUT_SIZE; ++i)
+             w_ih[tx + (INPUT_SIZE * i)] = w_hid[tx][i];
+    }
+
+    /* Copy the output layer's weight matrix out to global memory. */
+    if (tx < OUTPUT_SIZE) {
+        for (int i = 0; i < HIDDEN_SIZE; ++i)
+             w_ho[tx + (OUTPUT_SIZE * i)] = w_out[tx][i];
     }
 }
