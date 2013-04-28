e3a0ce6eb422f585fcc0d1a04f439daf300a0456
Kevin Reuter <fastaccord1@gmail.com>
2013-04-28 17:22:39 -0500

    Added code to allocate memory for the different references

--- main.c
+++ main.c
@@ -17,9 +17,24 @@ main(int argc, char *argv[]) {
 
     int count;
 
+
+    /* Reads the data and stores it to the memory space.
+     * Also returns count for later use.*/
     count = readdata( data );
 
+    /* Computes the sizes to allocate. */
     size_t input_size = (INPUT_SIZE * count) * sizeof(float);
+    size_t hidden_size = (HIDDEN_SIZE * count) * sizeof(float);
+    size_t output_size = (OUTPUT_SIZE * count) * sizeof(float);
+
+    /* Allocates the memory for the different floats */
+    expected = malloc(output_size);
+    w_ih = malloc(hidden_size);
+    theta_h = malloc(hidden_size);
+    w_ho = malloc(output_size);
+    theta_o = malloc(output_size);
+    output = malloc(output_size);
+
 
 
     return EXIT_SUCCESS;
