579401c1713ca580454764cb96e5598e3e2cdf30
Brian Guthrie <baguthrie@sbcglobal.net>
2013-05-04 19:06:11 -0500

    Move data read functions to main.c

--- main.c
+++ main.c
@@ -6,14 +6,16 @@
 #include <time.h>
 #include <unistd.h>
 
-#include "data.h"
 #include "backprop.h"
 #include "st.h"
 
+#define NUM_ROWS 150
+#define NUM_COLUMNS 4
+#define BUF_SZ 256
 
 /* Learning Rate */
 #define RATE 0.2
-
+/* Maximum average SSE value */
 #define SSE_MAX 0.01
 
 static void run_cuda(float*, int, float*, float*, float*, float*, float*);
@@ -23,6 +25,8 @@ static void init_weights(float*, size_t, size_t);
 static void init_theta(float*, size_t);
 static void dumpmat(float*, size_t, size_t);
 static void die(const char *);
+static int  readdata(float**);
+static int  readexpected(float**);
 
 int
 main(int argc, char *argv[])
@@ -191,4 +195,104 @@ static void
 die(const char *prefix) {
     perror(prefix);
     exit(EXIT_FAILURE);
+}
+
+/* Performs normalization on a data set read in row-major order, normalizes it,
+ * performs cross-validation, and transposes it to column-major order.
+ */
+static int
+readdata( float **data )
+{
+    FILE *file;
+    float *rmData; //Row Major Order
+    rmData = (float *) malloc(NUM_COLUMNS * NUM_ROWS * sizeof(float));
+
+    float maxValues[4];
+    float minValues[4];
+    int lineCount = 0;
+    int dataCount = 0;
+    int i, j;
+    file = fopen("data/iris.data", "r");
+    
+    //Read data from file and find max and min values for each column
+    if ( file != NULL)
+    {
+        char line [128];
+
+        while ( fgets ( line, sizeof line, file ) != NULL )
+        {
+            char* token = strtok(line, ",");
+            while (token) 
+            {
+                float temp = atof(token);
+                if(lineCount == 0)
+                {
+                    maxValues[dataCount] = temp;
+                    minValues[dataCount] = temp;
+                }
+                
+                if(maxValues[dataCount] < temp)
+                    maxValues[dataCount] = temp;
+
+                if(minValues[dataCount] > temp)
+                    minValues[dataCount] = temp;
+
+                rmData[(lineCount * NUM_COLUMNS) + dataCount] = temp;
+                token = strtok(NULL, ",");
+                dataCount++;
+            }
+            lineCount++;
+            dataCount = 0;
+        }
+
+        fclose ( file );
+
+        //Normalize Data
+        for(i = 0; i < lineCount; i++)
+        {
+            for(j = 0; j < 4; j++)
+            {
+                //Normalize
+                rmData[(i * NUM_COLUMNS) + j] = ((rmData[(i * NUM_COLUMNS) + j]
+                            - minValues[j])/(maxValues[j] - minValues[j]));
+            }
+        }
+        
+        *data = rmData;
+    }
+
+    return lineCount;
+}
+
+static int
+readexpected(float **expected) {
+    FILE *f;
+    char buf[BUF_SZ];
+    int fields, lines = 0;
+    float *data = (float *) malloc(NUM_COLUMNS * NUM_ROWS * sizeof(float));
+    if (data == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((f = fopen("data/iris.output", "r")) == NULL) {
+        free(data);
+        fprintf(stderr, "Could not open training data result file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(buf, BUF_SZ, f) != NULL) {
+        char *str = strtok(buf, ",");
+        fields = 0;
+        while (str && fields < OUTPUT_SIZE) {
+            data[(lines * OUTPUT_SIZE) + fields] = strtof(str, NULL);
+            str = strtok(NULL, ",");
+            ++fields;
+        }
+        ++lines;
+    }
+    fclose(f);
+
+    *expected = data;
+    return lines;
 }
